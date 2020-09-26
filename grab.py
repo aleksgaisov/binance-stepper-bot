6db5393f472dded4cc9979f6a96dac1b602cde26
Aleksandr Gaisov <aleksgaisov@net-c.com>
2020-09-26 10:28:00 +0200

    Initial commit

--- grab.py
+++ grab.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import datetime
-import time
 
 import base
 
@@ -10,92 +9,84 @@ import base
 # --- in
 # None
 # --- out
-# * float(asset_balance)
+# * float(free_asset_balance)
 # -----------------
-def asset_balance():
-    asset_balance = base.CLIENT.get_account_balance()['vol']['X' + base.TRADING_ASSET]  # Array of data about original asset balance
+def free_asset_balance():
+    asset_balance = base.CLIENT.get_asset_balance(asset=base.TRADING_ASSET)       # Array of data about original asset balance
+    free_asset_balance = float(asset_balance['free'])                   # Original balance of free asset
 
-    time.sleep(1)  # Delay to avoid "public call frequency exceeded" error
-
-    return float(asset_balance)
+    return free_asset_balance
 
 
 # Get current coin balance that is free to be utilized in trading
 # --- in
 # None
 # --- out
-# * float(coin_balance)
+# * float(free_coin_balance)
 # -----------------
-def coin_balance():
-    coin_balance = base.CLIENT.get_account_balance()['vol']['X' + base.TRADING_COIN]  # Array of data about original coin balance
+def free_coin_balance():
+    coin_balace = base.CLIENT.get_asset_balance(asset=base.TRADING_COIN)          # Array of data about original coin balance
+    free_coin_balance = float(coin_balace['free'])                      # Original balance of free coin
 
-    time.sleep(1)  # Delay to avoid "public call frequency exceeded" error
-
-    return float(coin_balance)
+    return free_coin_balance
 
 
 # Convert server-time to datetime object
 # --- in
 # * float(timestamp)
 # --- out
-# * datetime()
+# * fromtimestamp()
 # -----------------
 def _timestamp_to_date(timestamp):
     global CLIENT
 
-    return datetime.datetime.fromtimestamp(timestamp)
+    return datetime.datetime.fromtimestamp(timestamp/1000)
 
 
-# Count orders closed within last 24h
+# Count IDs of orders closed within last 24h
 # --- in
 # None
 # --- out
 # * int(count)
 # -----------------
 def num_closed_inday_offers():
-    server_time = int(base.CLIENT.get_server_time()[1])
-    from_date = _timestamp_to_date(server_time) - datetime.timedelta(days=1)
-    from_date = int(from_date.timestamp())
+    server_time = _timestamp_to_date(base.CLIENT.get_server_time()['serverTime'])  # 'To' date
+    from_date = server_time - datetime.timedelta(days=1)  # 'From' date
 
-    orders_arr = base.CLIENT.get_closed_orders(trades=False, start=int(from_date), end=int(server_time), closetime=None)[0]
+    orders_arr = base.CLIENT.get_all_orders(symbol=base.TRADING_PAIR, limit=500)
     decimal_places = len(str(base.LOT).split('.')[1])
 
     count = 0
-    for i in range(0, len(orders_arr)):
-        if (orders_arr['status'][i] == 'closed' and orders_arr['descr_type'][i] == 'sell'
-            and round(float(orders_arr['vol'][i]), decimal_places) == base.LOT):
+    for order in orders_arr:
+        if (order['status'] == 'FILLED' and order['side'] == 'SELL' and round(float(order['origQty']), decimal_places) == base.LOT
+                and (from_date < _timestamp_to_date(order['updateTime']) < server_time)):
 
             count += 1
-
-    time.sleep(1)  # Delay to avoid "public call frequency exceeded" error
     
-    return int(count)
+    return count
 
 
-# Count orders closed within a week
+# Count IDs of orders closed within a week
 # --- in
 # None
 # --- out
 # * int(count)
 # -----------------
 def num_closed_inweek_offers():
-    server_time = int(base.CLIENT.get_server_time()[1])
-    from_date = _timestamp_to_date(server_time) - datetime.timedelta(days=7)
-    from_date = int(from_date.timestamp())
+    server_time = _timestamp_to_date(base.CLIENT.get_server_time()['serverTime'])  # 'To' date
+    from_date = server_time - datetime.timedelta(days=7)  # 'From' date
 
-    orders_arr = base.CLIENT.get_closed_orders(trades=False, start=int(from_date), end=int(server_time), closetime=None)[0]
+    orders_arr = base.CLIENT.get_all_orders(symbol=base.TRADING_PAIR, limit=500)
     decimal_places = len(str(base.LOT).split('.')[1])
-
+    
     count = 0
-    for i in range(0, len(orders_arr)):
-        if (orders_arr['status'][i] == 'closed' and orders_arr['descr_type'][i] == 'sell'
-            and round(float(orders_arr['vol'][i]), decimal_places) == base.LOT):
+    for order in orders_arr:
+        if (order['status'] == 'FILLED' and order['side'] == 'SELL' and round(float(order['origQty']), decimal_places) == base.LOT
+            and (from_date < _timestamp_to_date(order['updateTime']) < server_time)):
 
             count += 1
-
-    time.sleep(1)  # Delay to avoid "public call frequency exceeded" error
     
-    return int(count)
+    return count
     
 
 # Get the number of the previous month 
@@ -114,27 +105,24 @@ def _get_past_month_num():
         return current_month_num - 1
 
 
-# Count orders, closed within a month
+# Count IDs of orders, closed within a month
 # --- in
 # None
 # --- out
 # * int(count)
 # -----------------
 def num_closed_inmonth_offers():
-    server_time = int(base.CLIENT.get_server_time()[1])
-    month_before_date = _timestamp_to_date(server_time).replace(month=_get_past_month_num())  # Date a month before
-    month_before_date = int(month_before_date.timestamp())
+    server_time = _timestamp_to_date(base.CLIENT.get_server_time()['serverTime'])  # 'To' date
+    month_before_date = server_time.replace(month=_get_past_month_num())  # Date a month before
 
-    orders_arr = base.CLIENT.get_closed_orders(trades=False, start=int(from_date), end=int(server_time), closetime=None)[0]
+    orders_arr = base.CLIENT.get_all_orders(symbol=base.TRADING_PAIR, limit=500)
     decimal_places = len(str(base.LOT).split('.')[1])
 
     count = 0
-    for i in range(0, len(orders_arr)):
-        if (orders_arr['status'][i] == 'closed' and orders_arr['descr_type'][i] == 'sell'
-            and round(float(orders_arr['vol'][i]), decimal_places) == base.LOT):
-
+    for order in orders_arr:
+        if (order['status'] == 'FILLED' and order['side'] == 'SELL' and round(float(order['origQty']), decimal_places) == base.LOT
+            and (month_before_date < _timestamp_to_date(order['updateTime']) < server_time)):
+        
             count += 1
-
-    time.sleep(1)  # Delay to avoid "public call frequency exceeded" error
     
-    return int(count)
+    return count
