c89d5c05f185d2e2221785f214c0490c729afc5c
Aleksandr Gaisov <aleksgaisov@net-c.com>
2020-09-26 10:27:40 +0200

    Merge config and base module

--- base.py
+++ base.py
@@ -12,15 +12,14 @@ import time
 import random
 import string
 
-import krakenex
-from pykrakenapi import KrakenAPI
-
 import find
 import grab
 import mail
 import save
 import send
 import test
+
+from binance.client import Client
 
 
 #  -------------------------
@@ -34,12 +33,8 @@ import test
 # * float(current_coin_price)
 # -----------------
 def _coin_price():
-    # Query ticker info for the given pair
-    ticker_info = CLIENT.get_ticker_information(TRADING_PAIR)
-    # Fetch last closed trade price
-    current_coin_price = ticker_info['c'][0][0]
-    # Delay to avoid "public call frequency exceeded" error
-    time.sleep(1)
+    # Query current coin price
+    current_coin_price = float(CLIENT.get_ticker(symbol=TRADING_PAIR)['askPrice'])
     
     return float(current_coin_price) 
 
@@ -85,33 +80,45 @@ def _generate_order_prices(coin_price):
 # --- out 
 # True or False
 # -----------------
-def _enough_balance(asset_balance_, coin_balance_): 
-    if (asset_balance_ >= LOT * _coin_price() and  # Is there enough coins to place at least one order
-        coin_balance_ >= LOT):
+def _enough_balance(free_asset_balance_, free_coin_balance_): 
+    if (free_asset_balance_ >= LOT * _coin_price() and  # Is there enough coins to place at least one order
+        free_coin_balance_ >= LOT):
         return True
     else:
         return False
 
 
-# Send BUY and SELL orders on the market
-# (also, return their IDs)
+# Generate a unique order id
+# (in both coins)
 # --- in 
-# * str(best_offer_price_)
-# * str(best_bid_price_)
+# * int(id_len)
 # --- out 
+# * str(order_id)
+# -----------------
+def _generate_order_id(id_len):
+    order_id = ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits) for i in range(id_len))
+
+    return order_id
+
+
+# Send BUY and SELL orders on the market
+# (also, check if they are not already there)
+# --- in 
+# * float(best_offer_price_)
+# * float(best_bid_price_)
 # * str(best_offer_id_)
 # * str(best_bid_id_)
+# --- out 
+# None
 # -----------------
-def _send_base_orders(best_offer_price_, best_bid_price_):
-    best_offer_id_ = send.base_order('sell', best_offer_price_)  # Send current best OFFER
+def _send_base_orders(best_offer_price_, best_bid_price_, best_offer_id_, best_bid_id_):
     print(f'| Placing best OFFER at price: {best_offer_price_}')
-    #print(f'| ID: {best_offer_id_}')
+    send.base_order('SELL', str(best_offer_price_), best_offer_id_)  # Send current best OFFER
 
-    best_bid_id_ = send.base_order('buy', best_bid_price_)  # Send current best BID
+    time.sleep(1)  # Delay
+
     print(f'| Placing best BID at price: {best_bid_price_}')
-    #print(f'| ID: {best_bid_id_}')
-
-    return str(best_offer_id_), str(best_bid_id_)
+    send.base_order('BUY', str(best_bid_price_), best_bid_id_)  # Send current bet BID
 
 
 #  -------------------------
@@ -119,22 +126,22 @@ def _send_base_orders(best_offer_price_, best_bid_price_):
 #  -------------------------
 
 #===== Config Values =====#
-API_KEY                 = str('MMv4hACEK44tlbaU108ITxXTHR0t2Q8jE1SDXwo0qH0arnanb1Qssbse') 
-SECRET_KEY              = str('433IQMRi7vLbsRuBXGDqdyNOkl6RAu2QL8OhrIlzAeKlAVgtyqejAzFpiJpFtHN5RJ0RZ76lrofIZMw0SGsjhg==')
+API_KEY                 = str('') 
+API_SECRET              = str('')
 
-TRADING_COIN            = str('DOT')                     # Trading coin (ex. ETH)
-TRADING_ASSET           = str('XBT')                     # Coin to trade against of (ex. BTC)
-STEP                    = float(0.00019)                # Distance between BUY (BID) and SELL (OFFER) orders in asset value
-SPREAD                  = int(3)                         # Distance between BID <=> OFFER in number of STEPS
-LOT                     = float(0.02)                    # Quantity of coins per order
- 
-MAIL_DAILY_REPORTS      = bool(True)                     # If you want daily generated reports to be send to your email 
-MAIL_WEEKLY_REPORTS     = bool(True)                     # If you want weekly generated reports to be send to your email 
-MAIL_MONTHLY_REPORTS    = bool(True)                     # If you want monthly generated reports to be send to your email 
+TRADING_COIN            = str('')                       # Trading coin (ex. BNB)
+TRADING_ASSET           = str('')                       # Coin to trade against of (ex. BTC)
+STEP                    = float()                       # Distance between BUY (BID) and SELL (OFFER) orders in asset value
+SPREAD                  = int()                         # Distance between BID <=> OFFER in number of STEPS
+LOT                     = float()                       # Quantity of coins per order
 
-SENDER_ADDRESS          = str('gaisovalex71@gmail.com')  # From email
-SENDER_PASS             = str('HU6xvxvJL32df')           # Email password
-RECEIVER_ADDRESS        = str('bowm4n@disroot.org')      # To email
+MAIL_DAILY_REPORTS      = bool(True)                    # If you want daily generated reports to be send to your email 
+MAIL_WEEKLY_REPORTS     = bool(True)                    # If you want weekly generated reports to be send to your email 
+MAIL_MONTHLY_REPORTS    = bool(True)                    # If you want monthly generated reports to be send to your email 
+
+SENDER_ADDRESS          = str('')                       # From email
+SENDER_PASS             = str('')                       # Email password
+RECEIVER_ADDRESS        = str('')                       # To email
 
 
 #  -------------------------
@@ -142,11 +149,10 @@ RECEIVER_ADDRESS        = str('bowm4n@disroot.org')      # To email
 #  -------------------------
 
 #===== Connecting to API =====#
-API = krakenex.API(API_KEY, SECRET_KEY)
-CLIENT = KrakenAPI(API, tier='Intermediate', retry=0, crl_sleep=0)
+CLIENT = Client(API_KEY, API_SECRET)  # Connecting to the Binance API
 
 #===== Market Values =====#
-TRADING_PAIR = TRADING_COIN + TRADING_ASSET  # Trading pair (ex. ETH for BTC => ETHBTC)
+TRADING_PAIR = TRADING_COIN + TRADING_ASSET  # Trading pair (ex. BNB for BTC => BNBBTC)
 DECIMAL_PLACES = _decimal_places(str(_coin_price()))  # Round all order prices to a constant number of decimal places
 ORDER_PRICES = _generate_order_prices(float(_coin_price()))  # Array of order prices
 
@@ -171,9 +177,12 @@ if __name__ == "__main__":
     best_offer_price = find.best_offer_price(float(window_price))  # Current best OFFER
     best_bid_price = find.best_bid_price(float(best_offer_price))  # Current best BID
 
+    best_offer_id = _generate_order_id(16)  # A unique identificator for OFFER
+    best_bid_id = _generate_order_id(16)  # A unique identificator for BID
+        
     #===== Init Trading =====#
     # If there is not enough balance to start trading
-    if not _enough_balance(float(grab.asset_balance()), float(grab.coin_balance())):  
+    if not _enough_balance(float(grab.free_asset_balance()), float(grab.free_coin_balance())):  
         print('\n| Not enough balance in asset or coin!')
         sys.exit()
 
@@ -185,10 +194,8 @@ if __name__ == "__main__":
     print(f'| Best BID: {best_bid_price}')
     print('| ----------\n')
 
-    # Send both, best OFFER and best BID on the market and recieve their unique IDs
-    base_order_ids = _send_base_orders(str(best_offer_price), str(best_bid_price))
-    best_offer_id = base_order_ids[0]  # Unique best OFFER ID
-    best_bid_id = base_order_ids[1]  # Unique best BID ID
+    # Send both, best OFFER and best BID on the market
+    _send_base_orders(str(best_offer_price), str(best_bid_price), str(best_offer_id), str(best_bid_id))
     print('')
 
 
@@ -210,11 +217,11 @@ if __name__ == "__main__":
             if not test.order_active(best_offer_id):
                 # Check if we have enough in asset and coin
                 # (we do not need to bother placing new orders if we do not have enough balace)
-                if _enough_balance(float(grab.asset_balance()), float(grab.coin_balance())):
+                if _enough_balance(float(grab.free_asset_balance()), float(grab.free_coin_balance())):
                     print(f'| @@@{datetime.datetime.now().strftime("%H:%M")}@@@')
                     print(f'| OFFER at price {best_offer_price} is Filled')
 
-                    save.order_record('sell', str(best_offer_price))  # Save the record about closed OFFER
+                    save.order_record('SELL', str(best_offer_price))  # Save the record about closed OFFER
 
                     print(f'| Cancelling BID at: {best_bid_price}')
                     send.cancel_order(str(best_bid_id))  # Cancel the pair BID (if still on the market)
@@ -223,10 +230,11 @@ if __name__ == "__main__":
                     best_offer_price = find.best_offer_price(float(window_price))  # New updated best OFFER price
                     best_bid_price = find.best_bid_price(float(best_offer_price))  # New updated best BID price
 
+                    best_offer_id = _generate_order_id(16)  # New unique identificator for OFFER
+                    best_bid_id = _generate_order_id(16)  # New unique identificator for BID
+
                     # Send new OFFER and BID on the market
-                    base_order_ids = _send_base_orders(str(best_offer_price), str(best_bid_price))
-                    best_offer_id = base_order_ids[0]  # Unique best OFFER ID
-                    best_bid_id = base_order_ids[1]  # Unique best BID ID
+                    _send_base_orders(str(best_offer_price), str(best_bid_price), str(best_offer_id), str(best_bid_id))
 
                     print('| @@@@@@@@@@@')
 
@@ -240,11 +248,11 @@ if __name__ == "__main__":
             if not test.order_active(best_bid_id): 
                 # Check if we have enough in asset and coin
                 # (we do not need to bother placing new orders if we do not have enough balace)
-                if _enough_balance(float(grab.asset_balance()), float(grab.coin_balance())):
+                if _enough_balance(float(grab.free_asset_balance()), float(grab.free_coin_balance())):
                     print(f'| ###{datetime.datetime.now().strftime("%H:%M")}###')
                     print(f'| BID at price {best_bid_price} is Filled')
 
-                    save.order_record('buy', str(best_bid_price))  # Save the record about closed BID
+                    save.order_record('BUY', str(best_bid_price))  # Save the record about closed BID
 
                     print(f'| Cancelling OFFER at: {best_offer_price}')
                     send.cancel_order(str(best_offer_id))  # Cancel the pair BID (if still on the market)
@@ -253,10 +261,11 @@ if __name__ == "__main__":
                     best_offer_price = find.best_offer_price(float(window_price))  # New updated best OFFER price
                     best_bid_price = find.best_bid_price(float(best_offer_price))  # New updated best BID price
 
+                    best_offer_id = _generate_order_id(16)  # New unique identificator for OFFER
+                    best_bid_id = _generate_order_id(16)  # New unique identificator for BID
+
                     # Send new OFFER and BID on the market
-                    base_order_ids = _send_base_orders(str(best_offer_price), str(best_bid_price))
-                    best_offer_id = base_order_ids[0]  # Unique best OFFER ID
-                    best_bid_id = base_order_ids[1]  # Unique best BID ID
+                    _send_base_orders(str(best_offer_price), str(best_bid_price), str(best_offer_id), str(best_bid_id))
 
                     print('| ###########')
 
@@ -269,7 +278,7 @@ if __name__ == "__main__":
             if current_day != tmp_day:  # If day has passed
                 # Update the value to start a new countdown
                 tmp_day = current_day
-            
+
                 if MAIL_DAILY_REPORTS:
                     # Generage a daily report and send via email (if selected)
                     save.daily_report(int(grab.num_closed_inday_offers()))
@@ -277,7 +286,7 @@ if __name__ == "__main__":
             if current_week != tmp_week:  # If week has passed
                 # Update the value to start a new countdown
                 tmp_week = current_week
-            
+
                 if MAIL_WEEKLY_REPORTS:
                     # Generage a weekly report and send via email (if selected)
                     save.weekly_report(int(grab.num_closed_inweek_offers()))
